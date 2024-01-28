da072a085d2f2b3395273db364666764a393c697
Fernando Alemán <fernandoaleman@mac.com>
2024-01-28 16:04:59 -0600

    feat: add which-key plugin

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -10,19 +10,6 @@ local keymap = vim.keymap -- for conciseness
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
--- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
-
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-
 -- visual
 keymap.set("v", "<", "<gv", { desc = "Stay in right indentation mode" }) -- right indentation
 keymap.set("v", ">", ">gv", { desc = "Stay in left indentation mode" }) -- left indentation
