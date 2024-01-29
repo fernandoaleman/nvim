c743b105bad9ac23d2e45e4bbdcec3fd7728efd0
Fernando Alemán <fernandoaleman@mac.com>
2024-01-29 08:59:33 -0600

    feat: add auto-session plugin

---
+++ lua/custom/plugins/auto-session.lua
@@ -0,0 +1,24 @@
+return {
+  "rmagatti/auto-session",
+  config = function()
+    local auto_session = require("auto-session")
+
+    auto_session.setup({
+      auto_restore_enabled = false,
+      auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
+    })
+
+    -- set keymaps
+    local mappings = {
+      ["a"] = {
+        name = "auto sessions",
+        r = { "<cmd>SessionRestore<CR>", "restore session for cwd" },
+        s = { "<cmd>SessionSave<CR>", "save session for auto session root dir" },
+      },
+    }
+
+    local which_key = require("which-key")
+
+    which_key.register(mappings, which_key_opts)
+  end,
+}
