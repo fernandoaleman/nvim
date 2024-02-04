66993c0856fcb3c57b3b0f59b7dff81c2c1e8832
Fernando Alemán <fernandoaleman@mac.com>
2024-02-04 13:54:11 -0600

    feat: add conform plugin

---
+++ lua/custom/plugins/formatting.lua
@@ -0,0 +1,35 @@
+return {
+  "stevearc/conform.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        css = { "prettier" },
+        html = { "prettier" },
+        javascript = { "prettier" },
+        json = { "prettier" },
+        lua = { "stylua" },
+        markdown = { "prettier" },
+        python = { "isort", "black" },
+        ruby = { "rubocop" },
+        yaml = { "prettier" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
