2763f9ae8d29e90ad33c014711e400b08b852f67
Fernando Alemán <fernandoaleman@mac.com>
2024-02-04 14:02:45 -0600

    feat: add nvim-lint plugin

---
+++ lua/custom/plugins/linting.lua
@@ -0,0 +1,37 @@
+return {
+  "mfussenegger/nvim-lint",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      python = { "pylint" },
+      ruby = { "rubocop" },
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+
+    -- set keymaps
+    local mappings = {
+      ["l"] = {
+        function()
+          lint.try_lint()
+        end,
+        "Trigger linting for current file",
+      },
+    }
+
+    local which_key = require("which-key")
+
+    which_key.register(mappings, which_key_opts)
+  end,
+}
