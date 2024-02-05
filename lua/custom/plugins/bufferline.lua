cefd5372879e48ed855d2d1f1ba94a741f88b19a
Fernando Alemán <fernandoaleman@mac.com>
2024-02-05 10:37:42 -0600

    feat: add bufferline keymaps for next and previous buffers

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -11,18 +11,21 @@ return {
         hover = {
           enabled = true,
           delay = 150,
-          reveal = {'close'}
-        }
+          reveal = { "close" },
+        },
       },
     })
+
+    vim.keymap.set("n", "<Tab>", "<cmd>bn<CR>") -- next buffer
+    vim.keymap.set("n", "<S-Tab>", "<cmd>bp<CR>") -- previous buffer
 
     -- set keymaps
     local mappings = {
       ["b"] = {
         name = "buffer",
         b = { "<cmd>Telescope buffers<CR>", "find buffers" },
-        c = { "<cmd>bdelete<CR>", "close buffer" },  -- close current file
-        k = { "<cmd>bdelete<CR>", "kill buffer" },  -- close current file
+        c = { "<cmd>bdelete<CR>", "close buffer" }, -- close current file
+        k = { "<cmd>bdelete<CR>", "kill buffer" }, -- close current file
         n = { "<cmd>bnext<CR>", "switch to next buffer" },
         o = { "<cmd>e #<CR>", "switch between last two buffers" },
         p = { "<cmd>bprevious<CR>", "switch to previous buffer" },
@@ -32,5 +35,5 @@ return {
     local which_key = require("which-key")
 
     which_key.register(mappings, which_key_opts)
-  end
+  end,
 }
