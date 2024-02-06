e3a399065881cca7510ab910513d89975e933c53
Fernando Alemán <fernandoaleman@mac.com>
2024-02-06 14:39:54 -0600

    feat: add keymaps for moving selected lines

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -13,3 +13,7 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- visual
 keymap.set("v", "<", "<gv", { desc = "Stay in right indentation mode" }) -- right indentation
 keymap.set("v", ">", ">gv", { desc = "Stay in left indentation mode" }) -- left indentation
+
+-- Move selected lines with shift+j or shift+k
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
