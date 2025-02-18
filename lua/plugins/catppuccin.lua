f30fa13e3be1f111e0401c6d4e95d5c8f0699ab3
Fernando Alemán <fernandoaleman@mac.com>
2025-02-17 18:36:17 -0600

    Install and configure catppuccin plugin

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+      styles = {
+        comments = { "italic" },
+      },
+    },
+  },
+}
