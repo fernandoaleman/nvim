d9d918b0b55b072225121c4a109f41db9770f89b
Fernando Alemán <fernandoaleman@mac.com>
2025-02-17 19:39:47 -0600

    Add custom keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- general
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- visual
+vim.keymap.set("v", "<", "<gv", { desc = "Stay in right indentation mode" }) -- right indentation
+vim.keymap.set("v", ">", ">gv", { desc = "Stay in left indentation mode" }) -- left indentation
