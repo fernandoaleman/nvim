e7df1d73d97ce1d14aaf1dc7bae64876f6fe9516
Fernando Alemán <fernandoaleman@mac.com>
2025-04-14 17:29:30 -0500

    Set unix file format
    This is helpful like in the case when working with other people that are
    using Windows computers that create a different carriage return ^M.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- File formats
+vim.opt.fileformats = { "unix", "mac" }
+
 -- Ruby
 vim.g.lazyvim_ruby_lsp = "solargraph"
 vim.g.lazyvim_ruby_formatter = "rubocop"
