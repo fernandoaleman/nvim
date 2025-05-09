37b4d68d8369baa2a59ee8ec9c9244cf7e80123b
Fernando Alemán <fernandoaleman@mac.com>
2025-05-09 16:12:20 -0500

    Update nvim-lspconfig so it does not override the entire nvim-lspconfig plugin

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,24 +2,24 @@ local util = require("lspconfig.util")
 
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      solargraph = {
-        cmd = { os.getenv("HOME") .. "/.asdf/shims/solargraph", "stdio" },
-        filetypes = { "ruby" },
-        root_dir = util.root_pattern("Gemfile", ".git", vim.fn.getcwd()),
-        settings = {
-          solargraph = {
-            autoformat = true,
-            completion = true,
-            diagnostic = true,
-            folding = true,
-            references = true,
-            rename = true,
-            symbols = true,
-          },
+  opts = function(_, opts)
+    opts.servers = opts.servers or {}
+
+    opts.servers.solargraph = {
+      cmd = { vim.fn.expand("$HOME/.asdf/shims/solargraph"), "stdio" },
+      filetypes = { "ruby" },
+      root_dir = util.root_pattern("Gemfile", ".git"),
+      settings = {
+        solargraph = {
+          autoformat = true,
+          completion = true,
+          diagnostic = true,
+          folding = true,
+          references = true,
+          rename = true,
+          symbols = true,
         },
       },
-    },
-  },
+    }
+  end,
 }
