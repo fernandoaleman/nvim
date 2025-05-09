adebebafc33abb4632f6cb5fd6966266bce01189
Fernando Alemán <fernandoaleman@mac.com>
2025-05-09 16:18:06 -0500

    Fix mason-lspconfig.nvim 2.0 breaking update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,13 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
+  -- Pin to version 1.0 until LazyVim is updated to account for
+  -- mason-lspconfig.nvim 2.0 update
+  -- Begin
+  version = "^1.0.0",
+  dependencies = {
+    { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  },
+  -- End
   opts = {
     ensure_installed = {
       "ansible-language-server",
