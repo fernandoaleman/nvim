a576814745778b2be98bd7123d7c6768f128c55e
Fernando Alemán <fernandoaleman@mac.com>
2025-06-21 18:18:57 -0500

    update mason plugin

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,17 +1,17 @@
 return {
-  "mason-org/mason.nvim",
-  -- Pin to version 1.0 until LazyVim is updated to account for
-  -- mason-lspconfig.nvim 2.0 update
-  -- Begin
-  version = "^1.0.0",
-  dependencies = {
-    { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-  },
-  -- End
-  opts = {
-    ensure_installed = {
-      "ansible-language-server",
-      "ansible-lint",
-    },
-  },
+  --   "mason-org/mason.nvim",
+  --   -- Pin to version 1.0 until LazyVim is updated to account for
+  --   -- mason-lspconfig.nvim 2.0 update
+  --   -- Begin
+  --   version = "^1.0.0",
+  --   dependencies = {
+  --     { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  --   },
+  --   -- End
+  --   opts = {
+  --     ensure_installed = {
+  --       "ansible-language-server",
+  --       "ansible-lint",
+  --     },
+  --   },
 }
