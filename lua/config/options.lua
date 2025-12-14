b885d27e134c461ad15b893c45f276c2a516f872
Fernando Alemán <fernandoaleman@mac.com>
2025-12-13 20:17:17 -0600

    Update config options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- disable swap files
+vim.opt.swapfile = false
+
+-- File formats
+vim.opt.fileformats = { "unix", "mac" }
