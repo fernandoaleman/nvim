91949af456ea8eb7b3325da110b4ee07e5f648b5
Fernando Alemán <fernandoaleman@mac.com>
2025-12-13 21:11:28 -0600

    Add lazydocker keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,8 @@ vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- visual
 vim.keymap.set("v", "<", "<gv", { desc = "Stay in right indentation mode" }) -- right indentation
 vim.keymap.set("v", ">", ">gv", { desc = "Stay in left indentation mode" }) -- left indentation
+
+-- lazydocker
+vim.keymap.set("n", "<leader>dd", function()
+  require("snacks").terminal({ "lazydocker" }, { env = { PWD = vim.fn.getcwd() } })
+end, { desc = "LazyDocker" })
